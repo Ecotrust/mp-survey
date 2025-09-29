3e6bcfc1500458d6fa83868d60f4797ea861a518
Ryan Hodges <ryan.d.hodges@gmail.com>
2025-09-29 16:41:45 -0700

    using 'get_myplanner_...' views to inject content into visualize via settings.PLANNER_APPS

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.template.loader import render_to_string
 
-# Create your views here.
+def get_myplanner_js(request):
+    js_tag = '<script src="/static/survey/js/survey_myplanner.js"></script>'
+    return js_tag
+
+def get_myplanner_html(request, template='survey/survey_myplanner.html'):
+    context = {}
+    rendered = render_to_string(template, context=context, request=request)
+    return rendered
+
+def get_myplanner_css(request):
+    return ''
