3ef1b14ae25082cbba74b9fff5d6608cb9bf9f32
Ryan Hodges <ryan.d.hodges@gmail.com>
2025-10-02 12:58:13 -0700

    building out survey flow logic in JS

--- survey/static/survey/js/survey_myplanner.js
+++ survey/static/survey/js/survey_myplanner.js
@@ -4,7 +4,7 @@ $( "#group-surveys-header" ).on( "click", function() {
     });
 });
 
-function showSurveyForm(surveyId) {
+function showSurveyForm() {
     app.viewModel.scenarios.externalForm(true);
     $("#myplanner-survey-dialog").show();
 }
@@ -12,4 +12,22 @@ function showSurveyForm(surveyId) {
 function hideSurveyForm() {
     app.viewModel.scenarios.externalForm(false);
     $("#myplanner-survey-dialog").hide();
+}
+
+function takeNewSurvey(surveyId){
+    // Populate form field with loader
+    $('#myplanner-survey-dialog-body').html(
+        '<h3>Loading new survey...</h3>'
+    );
+    showSurveyForm();
+    // AJAX call to get form HTML
+}
+
+function continueSurvey(responseId){
+    // Populate form field with loader
+    $('#myplanner-survey-dialog-body').html(
+        '<h3>Loading your survey...</h3>'
+    );
+    showSurveyForm();
+    // AJAX call to get form HTML
 }
